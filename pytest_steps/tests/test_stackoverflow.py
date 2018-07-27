ac0cf670ae913f4f39da3392ea5ca3b8faa9bc78
Sylvain Marie <sylvain.marie@schneider-electric.com>
2018-07-27 18:26:11 +0200

    Added tests corresponding to stack overflow answers

---
+++ pytest_steps/tests/test_stackoverflow.py
@@ -0,0 +1,34 @@
+from pytest_steps import test_steps, StepsDataHolder
+
+
+class mylib:
+    @classmethod
+    def get_a(cls):
+        return 'a'
+
+    @classmethod
+    def convert_a_to_b(cls, a):
+        return 'b'
+
+    @classmethod
+    def works_with(cls, a, b):
+        return True
+
+
+def step_first(steps_data):
+    steps_data.a = mylib.get_a()
+
+
+def step_conversion(steps_data):
+    steps_data.b = mylib.convert_a_to_b(steps_data.a)
+
+
+def step_a_works_with_b(steps_data):
+    assert mylib.works_with(steps_data.a, steps_data.b)
+
+
+@test_steps(step_first, step_conversion, step_a_works_with_b)
+def test_suite_with_shared_results(test_step, steps_data: StepsDataHolder):
+
+    # Execute the step with access to the steps_data holder
+    test_step(steps_data)
---
+++ pytest_steps/tests/test_stackoverflow2.py
@@ -0,0 +1,25 @@
+import random
+from pytest_steps import test_steps, depends_on
+
+
+def step_instance_start():
+    # assert random.choice((True, False))
+    assert True
+
+
+@depends_on(step_instance_start)
+def step_instance_stop():
+    # assert random.choice((True, False))
+    assert True
+
+
+@depends_on(step_instance_start)
+def step_instance_delete():
+    # assert random.choice((True, False))
+    assert True
+
+
+@test_steps(step_instance_start, step_instance_stop, step_instance_delete)
+def test_suite(test_step):
+    # Execute the step
+    test_step()
