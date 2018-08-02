676191a2820e2a86777ac0e5c11192c71f96c849
RFarkas <r4lv@peaxels.com>
2018-08-02 11:53:36 +0200

    add python 2.7 to Travis CI / setup.py

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         # 'Programming Language :: Python :: 2',
         # 'Programming Language :: Python :: 2.6',
-        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.7',
         # 'Programming Language :: Python :: 3',
         # 'Programming Language :: Python :: 3.3',
         # 'Programming Language :: Python :: 3.4',
