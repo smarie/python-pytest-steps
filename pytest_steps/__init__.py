293f487bc36e1b51c693ee764c03de6f2a60c8f1
Sylvain Marie <sylvain.marie@schneider-electric.com>
2018-12-17 22:09:40 +0100

    `steps_harvest_df_utils` submodule is now correctly listed in `__all__`

--- pytest_steps/__init__.py
+++ pytest_steps/__init__.py
@@ -16,10 +16,12 @@ from pytest_steps.steps_harvest_df_utils import pivot_steps_on_df, get_flattened
 __all__ = [
     # the submodules
     'steps', 'decorator_hack', 'steps_common', 'steps_generator', 'steps_parametrizer', 'steps_harvest',
+    'steps_harvest_df_utils',
     # all symbols imported above
     'test_steps',
     'StepsDataHolder', 'depends_on',
     'optional_step', 'one_per_step',
+    # harvest-related
     'handle_steps_in_results_dct', 'remove_step_from_test_id', 'get_all_pytest_param_names_except_step_id',
     'pivot_steps_on_df', 'get_flattened_multilevel_columns', 'flatten_multilevel_columns', 'handle_steps_in_results_df'
 ]
