77aea9a2d885e3402972746522a2f1a83268f08c
Sylvain MARIE <sylvain.marie@se.com>
2019-03-15 15:50:49 +0100

    Accelerating tests for non-deployment targets in travis: removed extra pass for coverage, and removed html generation (keeping junit though, because of the success threshold)

--- ci_tools/run_tests.sh
+++ ci_tools/run_tests.sh
@@ -18,11 +18,18 @@ trap "cleanup" INT TERM EXIT
 #    echo "pytest not found. Trying py.test"
 #fi
 
-# First the raw for coverage
-echo -e "\n\n****** Running tests : 1/2 RAW******\n\n"
-coverage run --source pytest_steps -m pytest -v pytest_steps/tests_raw/
-# python -m pytest --cov-report term-missing --cov=./pytest_steps -v pytest_steps/tests_raw/
+if [ "${TRAVIS_PYTHON_VERSION}" = "3.5" ]; then
+   # full
+   # First the raw for coverage
+    echo -e "\n\n****** Running tests : 1/2 RAW******\n\n"
+    coverage run --source pytest_steps -m pytest -v pytest_steps/tests_raw/
+    # python -m pytest --cov-report term-missing --cov=./pytest_steps -v pytest_steps/tests_raw/
 
-# Then the meta (appended)
-echo -e "\n\n****** Running tests : 2/2 META******\n\n"
-python -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./pytest_steps --cov-append -v pytest_steps/tests/
+    # Then the meta (appended)
+    echo -e "\n\n****** Running tests : 2/2 META******\n\n"
+    python -m pytest --junitxml=reports/junit/junit.xml --html=reports/junit/report.html --cov-report term-missing --cov=./pytest_steps --cov-append -v pytest_steps/tests/
+else
+   # faster - skip coverage and html report
+    echo -e "\n\n****** Running tests******\n\n"
+    python -m pytest --junitxml=reports/junit/junit.xml -v pytest_steps/tests/
+fi
